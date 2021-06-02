fe835ffd2aedb62ba1adc5fcf5c6ee384b5a11ad
Duong Minh Quang <qduongminh3@gmail.com>
2021-06-02 08:29:03 +0700

    Optimize code: merge divide and multiple, minus and add. Fix code at divide and multiple to handle with multiple clicks

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var resultScreen = document.getElementsByClassName("calc-result")[0];
 var numberButton = document.getElementsByClassName("num");
 var curNumber, preSign, curSign, preResult, curResult;
 function init() {
-    curResult = 0;
-    preResult = 0;
-    curNumber = "";
-    preSign = 3;
-    curSign = 3;
+  curResult = 0;
+  preResult = 0;
+  curNumber = "";
+  preSign = 3;
+  curSign = 3;
 }
 var sign = document.getElementsByClassName("sign");
 for (var x of buttons) {
@@ -34,9 +34,9 @@ document
   });
 // delete char
 document.getElementsByClassName("calc-button-arrow")[0].addEventListener("click", () => {
-    curNumber = curNumber.substr(1, curNumber.length-1);
-    if(curNumber==="") curNumber = 0;
-    resultScreen.innerHTML = curNumber;
+  curNumber = curNumber.substr(0, curNumber.length - 1);
+  if (curNumber === "") curNumber = "0";
+  resultScreen.innerHTML = curNumber;
 });
 // read number
 for (var i = 0; i < numberButton.length; i++) {
@@ -46,117 +46,74 @@ for (var i = 0; i < numberButton.length; i++) {
   });
 }
 // react when click sign
-// divide
-sign[0].addEventListener("click", function () {
-  if(curNumber == "") curNumber = 0;
-  switch (curSign) {
-    case 0:
-      curResult /= parseFloat(curNumber);
-      break;
-    case 1:
-      curResult *= parseFloat(curNumber);
-      break;
-    case 2:
-      curResult = parseFloat(curNumber);
-      preSign = 2;
-      break;
-    case 3:
-      curResult = parseFloat(curNumber);
-      preSign = 3;
+// divide and multiple
+var i;
+for (i = 0; i < 2; i++) {
+  sign[i].addEventListener("click", function () {
+    if (curNumber !== "") {
+    switch (curSign) {
+      case 0:
+        curResult /= parseFloat(curNumber);
+        break;
+      case 1:
+        curResult *= parseFloat(curNumber);
+        break;
+      case 2:
+        curResult = parseFloat(curNumber);
+        preSign = 2;
+        break;
+      case 3:
+        curResult = parseFloat(curNumber);
+        preSign = 3;
+    }
+    curNumber = "";
   }
-  curSign = 0;
-  curNumber = "";
-});
+  curSign = this.innerHTML === "÷" ? 0 : 1;
+  });
+}
 
-// multiple
-sign[1].addEventListener("click", function () {
-  if(curNumber == "") curNumber = 0;
-  switch (curSign) {
-    case 0:
-      curResult /= parseFloat(curNumber);
-      break;
-    case 1:
-      curResult *= parseFloat(curNumber);
-      break;
-    case 2:
-      curResult = parseFloat(curNumber);
-      preSign = 2;
-      break;
-    case 3:
-      curResult = parseFloat(curNumber);
-      preSign = 3;
-  }
-  curSign = 1;
-  curNumber = "";
-});
-
-// minus
-sign[2].addEventListener("click", function () {
-  if(curNumber == "") curNumber = 0;
-  switch (curSign) {
-    case 0:
-      curResult /= parseFloat(curNumber);
-      if(preSign === 2) preResult -= curResult;
-      else preResult += curResult;
-      break;
-    case 1:
-      curResult *= parseFloat(curNumber);
-      if(preSign === 2) preResult -= curResult;
-      else preResult += curResult;
-      break;
-    case 2:
-      preResult -= parseFloat(curNumber);
-      break;
-    case 3:
-      preResult += parseFloat(curNumber);
-  }
-  curResult = 0;
-  curSign = 2;
-  preSign = curSign;
-  curNumber = "";
-});
-
-// add
-sign[3].addEventListener("click", function () {
-  if(curNumber == "") curNumber = 0;
-  switch (curSign) {
-    case 0:
-      curResult /= parseFloat(curNumber);
-      if(preSign === 2) preResult -= curResult;
-      else preResult += curResult;
-      break;
-    case 1:
-      curResult *= parseFloat(curNumber);
-      if(preSign === 2) preResult -= curResult;
-      else preResult += curResult;
-      break;
-    case 2:
-      preResult -= parseFloat(curNumber);
-      break;
-    case 3:
-      preResult += parseFloat(curNumber);
-  }
-  curResult = 0;
-  curSign = 3;
-  preSign = curSign;
-  curNumber = "";
-});
-
+// minus and add
+for (i = 2; i < 4; i++) {
+  sign[i].addEventListener("click", function () {
+    if (curNumber === "") curNumber = "0";
+    switch (curSign) {
+      case 0:
+        curResult /= parseFloat(curNumber);
+        if (preSign === 2) preResult -= curResult;
+        else preResult += curResult;
+        break;
+      case 1:
+        curResult *= parseFloat(curNumber);
+        if (preSign === 2) preResult -= curResult;
+        else preResult += curResult;
+        break;
+      case 2:
+        preResult -= parseFloat(curNumber);
+        break;
+      case 3:
+        preResult += parseFloat(curNumber);
+    }
+    curResult = 0;
+    curSign = this.innerHTML === "-" ? 2 : 3;
+    preSign = curSign;
+    curNumber = "";
+  });
+}
 // click equal
 document
   .getElementsByClassName("calc-button-equal")[0]
   .addEventListener("click", function () {
-    if(curNumber == "") curNumber = 0;  
+    if (curNumber === "") curNumber = "0";
     switch (curSign) {
       case 0:
         curResult /= parseFloat(curNumber);
-        if(preSign === 2) preResult -= curResult;
-      else preResult += curResult;
+        if (preSign === 2) preResult -= curResult;
+        else preResult += curResult;
         break;
       case 1:
         curResult *= parseFloat(curNumber);
-        if(preSign === 2) preResult -= curResult;
-      else preResult += curResult;
+        if (preSign === 2) preResult -= curResult;
+        else preResult += curResult;
         break;
       case 2:
         preResult -= parseFloat(curNumber);
